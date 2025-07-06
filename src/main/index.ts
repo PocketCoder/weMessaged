0249b8d57e611a34b554112ea00f461fb42d6acf
jake <jake.williams77@gmail.com>
2025-07-06 09:13:01 +0100

    linting and staging

--- src/main/index.ts
+++ src/main/index.ts
@@ -95,7 +95,6 @@ ipcMain.handle(
       height: 670,
       show: false,
       autoHideMenuBar: true,
-      ...(process.platform === "linux" ? { icon } : {}),
       webPreferences: {
         preload: join(__dirname, "../preload/index.js"),
         sandbox: false,
