07ef0cedd233ff8f4f34141e03ecada463471870
jake <jake.williams77@gmail.com>
2025-07-06 08:15:57 +0100

    staging: format and lint

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,56 +1,53 @@
-import { app, shell, BrowserWindow, ipcMain, dialog } from 'electron';
-import os from 'os';
-import fs from 'fs';
-import { join } from 'path';
-import { existsSync } from 'fs';
-import Database, { Database as DatabaseType } from 'better-sqlite3';
-import { Message } from '../renderer/src/lib/types';
-import { electronApp, optimizer, is } from '@electron-toolkit/utils';
-import { askForFullDiskAccess, getAuthStatus } from 'node-mac-permissions';
-import icon from '../../resources/icon.png?asset';
-import { Event } from 'electron/main';
-import { convertAppleDateInt } from '../renderer/src/lib/utils';
+import { app, shell, BrowserWindow, ipcMain, dialog } from "electron";
+import os from "os";
+import fs from "fs";
+import { join } from "path";
+import { existsSync } from "fs";
+import Database, { Database as DatabaseType } from "better-sqlite3";
+import { Message } from "../renderer/src/lib/types";
+import { electronApp, optimizer, is } from "@electron-toolkit/utils";
+import { askForFullDiskAccess, getAuthStatus } from "node-mac-permissions";
+import icon from "../../resources/icon.png?asset";
+import { convertAppleDateInt } from "../renderer/src/lib/utils";
 
 let db: DatabaseType;
 
-ipcMain.handle('find-default', (): boolean => {
-	return existsSync(
-		`/Users/${os.userInfo().username}/Library/Messages/chat.db`
-	);
+ipcMain.handle("find-default", (): boolean => {
+  return existsSync(
+    `/Users/${os.userInfo().username}/Library/Messages/chat.db`,
+  );
 });
 
 ipcMain.handle(
-	'get-contacts',
-	(
-		event: Event
-	): { success: boolean; contacts?: { id: string }[]; error?: any } => {
-		try {
-			db = new Database(
-				`/Users/${os.userInfo().username}/Library/Messages/chat.db`,
-				{ fileMustExist: true }
-			);
-			const contacts = db.prepare('SELECT DISTINCT id FROM handle;').all() as {
-				id: string;
-			}[];
-			return { success: true, contacts: contacts };
-		} catch (err: any) {
-			return { success: false, error: err.message };
-		}
-	}
+  "get-contacts",
+  (): { success: boolean; contacts?: { id: string }[]; error?: unknown } => {
+    try {
+      db = new Database(
+        `/Users/${os.userInfo().username}/Library/Messages/chat.db`,
+        { fileMustExist: true },
+      );
+      const contacts = db.prepare("SELECT DISTINCT id FROM handle;").all() as {
+        id: string;
+      }[];
+      return { success: true, contacts: contacts };
+    } catch (err: unknown) {
+      return { success: false, error: err.message };
+    }
+  },
 );
 
 ipcMain.handle(
-	'get-messages',
-	(
-		event: Event,
-		contacts: String[]
-	): { success: boolean; messages?: Message[]; error?: any } => {
-		const placeholders = contacts.map(() => '?').join(',');
-		try {
-			if (contacts.length === 0) {
-				return { success: false, error: 'No contacts in array' };
-			}
-			const sql = `
+  "get-messages",
+  (
+    _,
+    contacts: string[],
+  ): { success: boolean; messages?: Message[]; error?: unknown } => {
+    const placeholders = contacts.map(() => "?").join(",");
+    try {
+      if (contacts.length === 0) {
+        return { success: false, error: "No contacts in array" };
+      }
+      const sql = `
 				SELECT
 					m.ROWID            AS message_id,
 					c.chat_identifier  AS other_party,
@@ -70,144 +67,144 @@ ipcMain.handle(
 				ORDER BY
 					m.date;
 			`;
-			const stmt = db.prepare(sql);
-			const messages = stmt.all(...contacts) as Message[];
-			const newMessages = messages.map((m) => ({
-				...m,
-				converted_date: convertAppleDateInt(m.apple_date_int),
-			}));
-			return { success: true, messages: newMessages };
-		} catch (err: any) {
-			console.log(err);
-			return { success: false, error: err.message };
-		}
-	}
+      const stmt = db.prepare(sql);
+      const messages = stmt.all(...contacts) as Message[];
+      const newMessages = messages.map((m) => ({
+        ...m,
+        converted_date: convertAppleDateInt(m.apple_date_int),
+      }));
+      return { success: true, messages: newMessages };
+    } catch (err: unknown) {
+      console.log(err);
+      return { success: false, error: err.message };
+    }
+  },
 );
 
 ipcMain.handle(
-	'generate-pdf',
-	(
-		event: Event,
-		data: { authors: string; title: string; acknowledgements: string },
-		messages: Message[]
-	) => {
-		const previewWindow = new BrowserWindow({
-			titleBarStyle: 'hidden',
-			...(process.platform !== 'darwin' ? { titleBarOverlay: true } : {}),
-			width: 900,
-			height: 670,
-			show: false,
-			autoHideMenuBar: true,
-			...(process.platform === 'linux' ? { icon } : {}),
-			webPreferences: {
-				preload: join(__dirname, '../preload/index.js'),
-				sandbox: false,
-				nodeIntegration: true,
-				contextIsolation: false,
-			},
-		});
-		previewWindow.on('ready-to-show', () => previewWindow.show());
-		if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-			previewWindow.loadURL(
-				`${process.env['ELECTRON_RENDERER_URL']}/src/document/book.html`
-			);
-		} else {
-			previewWindow.loadFile(join(__dirname, '../renderer/book.html'));
-		}
-		previewWindow.webContents.on('did-finish-load', () => {
-			previewWindow.webContents.send('pdf-data', data, messages);
-		});
-	}
+  "generate-pdf",
+  (
+    _,
+    data: { authors: string; title: string; acknowledgements: string },
+    messages: Message[],
+  ) => {
+    const previewWindow = new BrowserWindow({
+      titleBarStyle: "hidden",
+      ...(process.platform !== "darwin" ? { titleBarOverlay: true } : {}),
+      width: 900,
+      height: 670,
+      show: false,
+      autoHideMenuBar: true,
+      ...(process.platform === "linux" ? { icon } : {}),
+      webPreferences: {
+        preload: join(__dirname, "../preload/index.js"),
+        sandbox: false,
+        nodeIntegration: true,
+        contextIsolation: false,
+      },
+    });
+    previewWindow.on("ready-to-show", () => previewWindow.show());
+    if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+      previewWindow.loadURL(
+        `${process.env["ELECTRON_RENDERER_URL"]}/src/document/book.html`,
+      );
+    } else {
+      previewWindow.loadFile(join(__dirname, "../renderer/book.html"));
+    }
+    previewWindow.webContents.on("did-finish-load", () => {
+      previewWindow.webContents.send("pdf-data", data, messages);
+    });
+  },
 );
 
-ipcMain.handle('save-pdf', (event: Event, data: Uint8Array) => {
-	const saveLoc = dialog.showSaveDialogSync({
-		properties: ['createDirectory'],
-		defaultPath: 'iMessage-book.pdf'
-	});
+ipcMain.handle("save-pdf", (_, data: Uint8Array) => {
+  const saveLoc = dialog.showSaveDialogSync({
+    properties: ["createDirectory"],
+    defaultPath: "iMessage-book.pdf",
+  });
 
-	if (saveLoc) {
-		try {
-			fs.writeFileSync(saveLoc, Buffer.from(data));
-		} catch (e: any) {
-			dialog.showErrorBox('Error saving file', e.message);
-			console.error(e);
-		}
-	}
+  if (saveLoc) {
+    try {
+      fs.writeFileSync(saveLoc, Buffer.from(data));
+    } catch (e: unknown) {
+      dialog.showErrorBox("Error saving file", e.message);
+      console.error(e);
+    }
+  }
 });
 
 function createWindow(): void {
-	// Create the browser window.
-	const mainWindow = new BrowserWindow({
-		titleBarStyle: 'hidden',
-		...(process.platform !== 'darwin' ? { titleBarOverlay: true } : {}),
-		width: 900,
-		height: 670,
-		show: false,
-		autoHideMenuBar: true,
-		...(process.platform === 'linux' ? { icon } : {}),
-		webPreferences: {
-			preload: join(__dirname, '../preload/index.js'),
-			sandbox: false,
-		},
-	});
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    titleBarStyle: "hidden",
+    ...(process.platform !== "darwin" ? { titleBarOverlay: true } : {}),
+    width: 900,
+    height: 670,
+    show: false,
+    autoHideMenuBar: true,
+    ...(process.platform === "linux" ? { icon } : {}),
+    webPreferences: {
+      preload: join(__dirname, "../preload/index.js"),
+      sandbox: false,
+    },
+  });
 
-	mainWindow.on('ready-to-show', () => {
-		mainWindow.show();
-	});
+  mainWindow.on("ready-to-show", () => {
+    mainWindow.show();
+  });
 
-	mainWindow.webContents.setWindowOpenHandler((details) => {
-		shell.openExternal(details.url);
-		return { action: 'deny' };
-	});
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url);
+    return { action: "deny" };
+  });
 
-	// HMR for renderer base on electron-vite cli.
-	// Load the remote URL for development or the local html file for production.
-	if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-		mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
-	} else {
-		mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
-	}
+  // HMR for renderer base on electron-vite cli.
+  // Load the remote URL for development or the local html file for production.
+  if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+    mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
+  } else {
+    mainWindow.loadFile(join(__dirname, "../renderer/index.html"));
+  }
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-	// Set app user model id for windows
-	electronApp.setAppUserModelId('com.electron');
+  // Set app user model id for windows
+  electronApp.setAppUserModelId("com.electron");
 
-	if (process.platform === 'darwin') {
-		const status = getAuthStatus('full-disk-access');
-		console.log(`Status: ${status}`);
-		if (status === 'not determined' || status === 'denied') {
-			askForFullDiskAccess();
-		}
-	}
+  if (process.platform === "darwin") {
+    const status = getAuthStatus("full-disk-access");
+    console.log(`Status: ${status}`);
+    if (status === "not determined" || status === "denied") {
+      askForFullDiskAccess();
+    }
+  }
 
-	// Default open or close DevTools by F12 in development
-	// and ignore CommandOrControl + R in production.
-	// see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-	app.on('browser-window-created', (_, window) => {
-		optimizer.watchWindowShortcuts(window);
-	});
+  // Default open or close DevTools by F12 in development
+  // and ignore CommandOrControl + R in production.
+  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  app.on("browser-window-created", (_, window) => {
+    optimizer.watchWindowShortcuts(window);
+  });
 
-	createWindow();
+  createWindow();
 
-	app.on('activate', function () {
-		// On macOS it's common to re-create a window in the app when the
-		// dock icon is clicked and there are no other windows open.
-		if (BrowserWindow.getAllWindows().length === 0) createWindow();
-	});
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,120 +1,121 @@
-import folder from './assets/folder.svg';
-import computer from './assets/computer.svg';
-import write from './assets/write.svg';
-import { useEffect, useState } from 'react';
-import Select, { MultiValue } from 'react-select';
+import folder from "./assets/folder.svg";
+import computer from "./assets/computer.svg";
+import write from "./assets/write.svg";
+import { useEffect, useState } from "react";
+import Select, { MultiValue } from "react-select";
 
 type ContactOption = {
-	value: string;
-	label: string;
+  value: string;
+  label: string;
 };
 
 function App(): React.JSX.Element {
-	const [exists, setExists] = useState<boolean | null>(null);
-	const [loading, setLoading] = useState<boolean>(false);
-	const [phoneNumbers, setPhoneNumbers] = useState<ContactOption[]>([]);
-	const [selectedContacts, setSelectedContacts] = useState<ContactOption[]>([]);
+  const [exists, setExists] = useState<boolean | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [phoneNumbers, setPhoneNumbers] = useState<ContactOption[]>([]);
+  const [selectedContacts, setSelectedContacts] = useState<ContactOption[]>([]);
 
-	async function findDefault(): Promise<boolean> {
-		return await window.electron.ipcRenderer.invoke('find-default');
-	}
+  async function findDefault(): Promise<boolean> {
+    return await window.electron.ipcRenderer.invoke("find-default");
+  }
 
-	async function handleDefaultClick(): Promise<void> {
-		setLoading(true);
-		const result = await window.electron.ipcRenderer.invoke('get-contacts');
-		setLoading(false);
-		if (result.success) {
-			const options = result.contacts.map((item: { id: string }) => ({
-				value: item.id,
-				label: item.id,
-			}));
-			setPhoneNumbers(options);
-		} else {
-			console.error('Error reading file:', result.error);
-		}
-	}
+  async function handleDefaultClick(): Promise<void> {
+    setLoading(true);
+    const result = await window.electron.ipcRenderer.invoke("get-contacts");
+    setLoading(false);
+    if (result.success) {
+      const options = result.contacts.map((item: { id: string }) => ({
+        value: item.id,
+        label: item.id,
+      }));
+      setPhoneNumbers(options);
+    } else {
+      console.error("Error reading file:", result.error);
+    }
+  }
 
-	async function handleCreateClick(): Promise<void> {
-		if (selectedContacts.length === 0) {
-			alert('Please select at least one contact.');
-			return;
-		}
-		const contactIds = selectedContacts.map((contact) => contact.value);
-		setLoading(true);
-		const result = await window.electron.ipcRenderer.invoke(
-			'get-messages',
-			contactIds
-		);
-		setLoading(false);
-		if (!result.success) console.error('Error getting messages:', result.error);
-		try {
-			await window.electron.ipcRenderer.invoke(
-				'generate-pdf',
-				{
-					authors: 'Jake',
-					title: 'Test title',
-					acknowledgements: 'Test Acknowledgements.',
-				},
-				result.messages
-			);
-		} catch (error) {
-			console.error('Error invoking PDF generation:', error);
-		}
-	}
+  async function handleCreateClick(): Promise<void> {
+    if (selectedContacts.length === 0) {
+      alert("Please select at least one contact.");
+      return;
+    }
+    const contactIds = selectedContacts.map((contact) => contact.value);
+    setLoading(true);
+    const result = await window.electron.ipcRenderer.invoke(
+      "get-messages",
+      contactIds,
+    );
+    setLoading(false);
+    if (!result.success) console.error("Error getting messages:", result.error);
+    try {
+      await window.electron.ipcRenderer.invoke(
+        "generate-pdf",
+        {
+          authors: "Jake",
+          title: "Test title",
+          acknowledgements: "Test Acknowledgements.",
+        },
+        result.messages,
+      );
+    } catch (error) {
+      console.error("Error invoking PDF generation:", error);
+    }
+  }
 
-	useEffect(() => {
-		const checkDefault = async () => {
-			const result = await findDefault();
-			setExists(result);
-		};
-		checkDefault();
-	}, []);
+  useEffect(() => {
+    const checkDefault = async (): Promise<void> => {
+      const result = await findDefault();
+      setExists(result);
+    };
+    checkDefault();
+  }, []);
 
-	return (
-		<>
-			{phoneNumbers.length === 0 ?
-				<span>1. Select a database to begin...</span>
-			:	<></>}
-			<div id="button-container">
-				<button
-					className={'left'}
-					onClick={handleDefaultClick}
-					disabled={!exists}
-				>
-					<img src={computer} width="32px" height="32px" alt="computer" />
-					Default Database
-				</button>
-				<button className="right" disabled>
-					Choose Backup File
-					<img src={folder} width="32px" height="32px" alt="folder" />
-				</button>
-			</div>
-			{phoneNumbers.length === 0 ?
-				<></>
-			:	<div id="choose-contacts">
-					<span>2. Choose the contacts to include in your book...</span>
-					<div id="create-container">
-						<Select
-							isMulti
-							name="contacts"
-							options={phoneNumbers}
-							classNamePrefix={'select'}
-							onChange={(selected: MultiValue<ContactOption>) =>
-								setSelectedContacts(selected as ContactOption[])
-							}
-						/>
-						<button onClick={handleCreateClick}>
-							Create
-							<img src={write} width={'21px'} height={'21px'} alt="Pen" />
-						</button>
-					</div>
-				</div>
-			}
-			{loading ?
-				<span>Loading...</span>
-			:	<></>}
-		</>
-	);
+  return (
+    <>
+      {phoneNumbers.length === 0 ? (
+        <span>1. Select a database to begin...</span>
+      ) : (
+        <></>
+      )}
+      <div id="button-container">
+        <button
+          className={"left"}
+          onClick={handleDefaultClick}
+          disabled={!exists}
+        >
+          <img src={computer} width="32px" height="32px" alt="computer" />
+          Default Database
+        </button>
+        <button className="right" disabled>
+          Choose Backup File
+          <img src={folder} width="32px" height="32px" alt="folder" />
+        </button>
+      </div>
+      {phoneNumbers.length === 0 ? (
+        <></>
+      ) : (
+        <div id="choose-contacts">
+          <span>2. Choose the contacts to include in your book...</span>
+          <div id="create-container">
+            <Select
+              isMulti
+              name="contacts"
+              options={phoneNumbers}
+              classNamePrefix={"select"}
+              onChange={(selected: MultiValue<ContactOption>) =>
+                setSelectedContacts(selected as ContactOption[])
+              }
+            />
+            <button onClick={handleCreateClick}>
+              Create
+              <img src={write} width={"21px"} height={"21px"} alt="Pen" />
+            </button>
+          </div>
+        </div>
+      )}
+      {loading ? <span>Loading...</span> : <></>}
+    </>
+  );
 }
 
 export default App;
